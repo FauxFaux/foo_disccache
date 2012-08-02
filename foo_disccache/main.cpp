95a2a88005c41510f57749eb46b282f87e503fe9
Chris West (Faux) <git@goeswhere.com>
2012-08-02 17:02:47 +0100

    Compiles

---
+++ foo_disccache/main.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+
+DECLARE_COMPONENT_VERSION("Disc cache warmer", "0.0", "");
+
+
+// This will prevent users from renaming your component around (important for proper troubleshooter behaviors) or loading multiple instances of it.
+VALIDATE_COMPONENT_FILENAME("foo_disccache.dll");
