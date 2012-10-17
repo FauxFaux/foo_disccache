d0489f5c8f8aa93b17251ddb5aa29ddf0e560396
Chris West (Faux) <git@goeswhere.com>
2012-10-17 21:20:53 +0100

    version bump

--- foo_disccache/main.cpp
+++ foo_disccache/main.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 
-DECLARE_COMPONENT_VERSION("Disc cache warmer", "0.0", "");
+DECLARE_COMPONENT_VERSION("Disc cache warmer", "0.1", "");
 
 
 // This will prevent users from renaming your component around (important for proper troubleshooter behaviors) or loading multiple instances of it.
